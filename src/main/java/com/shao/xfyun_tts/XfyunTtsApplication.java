38fe711d19028a3ae65e04f293146e54efb5b606
shaohuailin <1978712753@qq.com>
2020-04-16 14:57:49 +0800

    init

---
+++ src/main/java/com/shao/xfyun_tts/XfyunTtsApplication.java
@@ -0,0 +1,13 @@
+package com.shao.xfyun_tts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XfyunTtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XfyunTtsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shao/xfyun_tts/XfyunTtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shao.xfyun_tts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XfyunTtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
